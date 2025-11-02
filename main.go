61ac54f76d6f62668d678003a9c1eb1def65ad13
Amit Upadhyay <amitu2016@gmail.com>
2025-11-02 21:27:18 +0530

    variables assignment

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 
-	var card string = "Ace of Spades"
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
 	fmt.Println(card)
 
 }
