b44ce87023971c69b2004e1451999a323e712a31
Amit Upadhyay <amitu2016@gmail.com>
2025-11-04 17:19:10 +0530

    functions

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Hearts")
-	fmt.Println(cards)
-}
 
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
